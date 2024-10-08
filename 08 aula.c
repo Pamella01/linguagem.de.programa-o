040af33296d6a23b8b1ba0ef88735341b9209cf8
Pamella <pamellabrenda01@gmail.com>
2024-10-08 09:45:21 -0300

    Add files via upload

---
+++ 08 aula.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+    int idadeFernando = 17;
+    int idadeDanilo = 18;
+
+    if(idadeFernando > idadeDanilo){
+        printf("Fernando e mais velho que Danilo!");
+    } else {
+         printf("Danilo e mais velho que Fernando!");
+}
+}
